fb80e1b87654a295960f9eb8d7d0200d2689c333
Oleh Prokopenko <olehprokopenko1@gmail.com>
2025-11-20 23:51:58 +0200

    add tusk solition

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const mainImg = document.getElementById('largeImg');
+const images = document.getElementById('thumbs');
+
+images.addEventListener('click', (ev) => {
+  ev.preventDefault();
+
+  const img = ev.target.closest('.list-item a img');
+  const srcValue = img.getAttribute('src');
+
+  mainImg.setAttribute('src', srcValue);
+});
