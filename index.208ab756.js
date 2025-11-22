8f4081d38088c87cf1596fb9c6458cb09c4c2ea4
Oleh Prokopenko <olehprokopenko1@gmail.com>
2025-11-22 19:25:38 +0200

    make build

---
+++ index.208ab756.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.getElementById("thumbs").addEventListener("click",function(t){t.preventDefault();var r=t.target.closest("a");if(r){var c=r.querySelector("img").getAttribute("src");e.setAttribute("src",c)}});
+//# sourceMappingURL=index.208ab756.js.map
